ce5c67acd8677816796313c69e6922edf5502232
NGUYEN THANH HA Specialist/System Operations Team <lgcns.ntha7@lgcns.com>
2024-06-19 18:44:39 -0700

    hh

---
+++ model_save.py
@@ -0,0 +1,12 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+# Load the pre-trained model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
+model = AutoModelForMaskedLM.from_pretrained("google-bert/bert-base-uncased")
+
+# Specify a path for saving the state_dict
+PATH = "model_state_dict.pt"
+
+# Save the state_dict
+torch.save(model.state_dict(), PATH)
